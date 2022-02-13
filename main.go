a0c110c003cabc4f126dcdc77e0e79ba33e77d67
yangtongchun <m15169658896@163.com>
2022-02-13 14:48:23 +0800

    添加代码

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"go-travel-blog/global"
+	"go-travel-blog/internal/model"
+	"go-travel-blog/internal/routers"
+	"go-travel-blog/pkg/setting"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
+
+func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout * time.Second,
+		WriteTimeout:   global.ServerSetting.WriteTimeout * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		return
+	}
+}
+
+func setupSetting() error {
+	newSetting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	err = newSetting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = newSetting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = newSetting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
